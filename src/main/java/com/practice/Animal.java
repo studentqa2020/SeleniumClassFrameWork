aa3396582eb970934fc64c5dc86fded31f383493
studetnqa2020 <sarowerqa@gmail.com>
2020-05-10 11:36:50 -0400

    Added my 1st code

---
+++ src/main/java/com/practice/Animal.java
@@ -0,0 +1,17 @@
+package com.practice;
+
+public class Animal {
+	
+	public void getsalary() {
+		
+		System.out.println("Hi");
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Animal obj = new Animal();
+		obj.getsalary();
+	}
+
+}
